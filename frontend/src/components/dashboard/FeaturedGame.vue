b7c8b40b252c3600786e329dfd42544969160b06
Diego <diegojrq@gmail.com>
2023-10-09 15:32:17 -0300

    Fixing layout and adding evaluation

--- frontend/src/components/dashboard/FeaturedGame.vue
+++ frontend/src/components/dashboard/FeaturedGame.vue
@@ -34,26 +34,38 @@
           </v-col>
           <v-col cols="12" md="10">
             <v-row>
-              <v-col cols="12" md="6">
-                <div class="div-game-name">
-                  <p>{{ game.name }}</p>
-                  <p>{{ formatDate(game.first_release_date) + ' (' + formatDateFromNow(game.first_release_date) + ')' }} </p>
-                  <p>{{ game.name }}</p>
-                  <p>{{ game.name }}</p>
-                  <p>{{ game.name }}</p>
-                  <p>{{ game.name }}</p>
-                  <p>{{ game.name }}</p>
+              <v-col cols="12" md="8">
+                <div class="div-game-info">
+                  <div class="div-game-name">
+                    <p>{{ game.name }}</p>
+                  </div>
+                  <div class="div-game-launch">
+                    <p>{{ 'launched on ' + formatDate(game.first_release_date) + ' (' + formatDateFromNow(game.first_release_date) + ')' }} </p>
+                    <p v-for="involved_company in game.involved_companies" :key="involved_company.id">
+                      {{ involved_company.publisher ? 'published by ' + involved_company.company.name : '' }}
+                    </p>
+                  </div>                  
                 </div>            
               </v-col>
-              <v-col cols="12" md="6">
-                <p>release date</p>
+              <v-col cols="12" md="4">
+                <v-progress-circular
+                  :rotate="360"
+                  :size="100"
+                  :width="12"
+                  :model-value="rating_value"
+                  :color="`${ game.rating > 75 ? 'green' : (game.rating > 50 ? 'yellow' : (game.rating > 25 ? 'orange' : 'red')) }`"
+                >
+                  <div class="div-game-rating-number">
+                    <p>
+                      {{ Number(game.rating.toFixed(0)) }}
+                    </p>
+                  </div>
+                </v-progress-circular>
+                
               </v-col>
             </v-row>
-    
           </v-col>
-        </v-row>   
-        
-  
+        </v-row>  
         <v-card-text v-show="!loading" class="v-card-text">
           <p>{{ game.summary }}</p>
         </v-card-text>
@@ -80,14 +92,25 @@ export default {
       game: [],
       loading: true,
       loaded: false,
+      rating_interval: {},
+      rating_value: 0
     }
+  },
+  beforeUnmount () {
+    clearInterval(this.interval)
   },
   created() {
     getGame(148241).then((response) => {
       this.game = response.data;
       this.loaded = true;
       this.loading = false;
-      let a = moment("2012-02", "YYYY-MM").daysInMonth();
+      
+      this.rating_interval = setInterval(() => {
+        if (this.rating_value >= this.game.rating) {
+          return 1;
+        }
+        this.rating_value += 1
+      }, 10);      
 
     });
   },
@@ -103,7 +126,7 @@ export default {
 
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
   .v-card {    
     margin-top: 20px;
     background-color: #333;
@@ -118,8 +141,6 @@ export default {
     border: 1px solid #fff;
     border-radius: 1%;
   }
-
-
 
   .div-title {
     width: 80%;
@@ -138,16 +159,32 @@ export default {
     font-size: 10px;
   }
   
-  .div-game-name {
+  .div-game-info {
     text-align: left;
-    padding: 1%;
     font-size: 1em;
   }
+
+  .div-game-name {    
+    padding-left: 4%;
+    font-size: 1.8em;
+  }
+
+  .div-game-rating-number {
+    font-size: 1.8em;
+    color: #fff;
+
+  }
+
+  .div-game-launch {    
+    padding-left: 8%;
+    font-size: 1.0em;
+    color: #999;
+  }
+
   .div-featured-game {
     text-align: right;
     padding: 1%;
     font-size: 10px;
   }
-  
 
 </style>
